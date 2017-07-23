9ec3092dcacc0cdd20bd63386903bab074701045
Colleen Cousins <colleencousins@gmail.com>
2017-07-23 11:34:03 -0500

    Add files via upload

---
+++ CS- Fundamental Programming (C++)/CS1136 Lab 2 Exercise 1 Project/Lab2_Exercise1.cpp
@@ -0,0 +1,14 @@
+// CS 1136 Lab 2 Exercise 1
+// Program by: Colleen Cousins
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   // Displays favorite color and food
+	cout << " My favorite color: blue" << endl;
+	cout << " My favorite food: sushi" << endl;
+
+	return 0;
+}
